bf49f88137e30ac83530b69e7f7ad0498655a1e1
sharad2605 <sharad26shukla@gmail.com>
2026-09-21 13:15:34 +0530

    4 table created

--- index.js
+++ index.js
@@ -20,20 +20,69 @@ connection.connect((err) => {
     
     console.log('Connected to the database');
     const createTableQuery = `
-        CREATE TABLE IF NOT EXISTS Students (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            email VARCHAR(255) NOT NULL UNIQUE
-        )
-    `;
+    CREATE TABLE IF NOT EXISTS Users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL UNIQUE
+    )
+`;
 
-    connection.execute(createTableQuery, (err) => { 
-            if (err) {
-                console.error('Error creating table:', err);
-                return;
-            }
+connection.execute(createTableQuery, (err) => {
+    if (err) {
+        console.error('Error creating Users table:', err);
+        return;
+    }
 
-            console.log('Table created successfully');
+    console.log('Users table created successfully');
+});
+
+const createTableBusesQuery = `
+    CREATE TABLE IF NOT EXISTS Buses (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        busNumber VARCHAR(255) NOT NULL,
+        totalSeats VARCHAR(255) NOT NULL UNIQUE,
+        availableSeats VARCHAR(255) NOT NULL UNIQUE
+    )
+`;
+
+connection.execute(createTableBusesQuery, (err) => {
+    if (err) {
+        console.error('Error creating Buses table:', err);
+        return;
+    }
+
+    console.log('Buses table created successfully');
+});
+const createTableBookingQuery = `
+    CREATE TABLE IF NOT EXISTS Bookings (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        seatNumber VARCHAR(255) NOT NULL
+    )
+`;
+
+connection.execute(createTableBookingQuery, (err) => {
+    if (err) {
+        console.error('Error creating Bookings table:', err);
+        return;
+    }
+
+    console.log('Bookings table created successfully');
+});
+const createTablePaymentQuery = `
+    CREATE TABLE IF NOT EXISTS Payments (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        amountPaid DECIMAL(10, 2) NOT NULL,
+        paymentStatus VARCHAR(255) NOT NULL UNIQUE
+    )
+`;
+
+connection.execute(createTablePaymentQuery, (err) => {
+    if (err) {
+        console.error('Error creating Payment table:', err);
+        return;
+    }
+
+    console.log('Payment table created successfully');
 });
 
 })
